a36903a1e0c87fbbf67e3bd6d4a3b611c0251905
Quenio dos Santos <quenio@me.com>
2015-11-28 11:36:09 -0200

    Implemented functio to visualize the data set in 3D.

---
+++ data_view.m
@@ -0,0 +1,19 @@
+function data_view(data, start_row, targets)
+    grid on; hold on;
+    data_size = size(data,2);
+    j = start_row - 1;
+    for i=1:data_size
+        plot3(data(j+1,i),data(j+2,i),data(j+3,i),line_spec(targets, i));
+    end
+    view(3)
+end
+
+function ls = line_spec(targets, column)
+    if targets(1,column) == 1
+        ls = 'b*';
+    elseif targets(2,column) == 1
+        ls = 'co';
+    else
+        ls = 'mx';
+    end    
+end    
